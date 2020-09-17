4d7d60e0b26730c166365590dace055285b99488
kirill <gerasev.kirill@gmail.com>
2020-09-17 16:37:16 +0300

    [*] setup

--- setup.py
+++ setup.py
@@ -2,29 +2,30 @@ import os
 from setuptools import find_packages, setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-        README = readme.read()
+    README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-atlassian',
-    version='0.1.4',
+    name='atlassian-connect-django',
+    version='0.2.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
     description='Django app for interact with atlassian libraries such as JIRA and Confluence.',
     long_description=README,
-    url='https://www.fluendo.com/',
-    author='Fluendo',
-    author_email='web-dev@fluendo.com',
+    author='Gerasev Kirill, Fluendo',
+    author_email='gerasev.kirill@gmail.com',
     install_requires=[
         "Django >= 1.11",
         "PyJWT >= 1.6.4",
         "atlassian-jwt >= 1.8.1",
         "requests >= 2.18.4",
         "requests-jwt==0.5.3",
-        "jira==2.0.0"],
+        "jira==2.0.0",
+        "jsmin >= 2.2.2"
+    ],
     classifiers=[
             'Environment :: Web Environment',
             'Framework :: Django',
