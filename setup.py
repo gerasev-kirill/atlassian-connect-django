a9850c023f01a4dfb707c7a9a29c09963680275c
kirill <gerasev.kirill@gmail.com>
2020-12-23 15:49:06 +0200

    [*] PyJWT version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author_email='gerasev.kirill@gmail.com',
     install_requires=[
         "Django >= 1.11",
-        "PyJWT >= 1.6.4",
+        "PyJWT >= 1.6.4, <= 1.7.1",
         "atlassian-jwt >= 1.8.1",
         "requests >= 2.18.4",
         "requests-jwt==0.5.3",
